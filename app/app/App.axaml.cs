778d52607d9d42e194a24548b76ca1bfefa2da9f
dex1fy <idarzamasova@yandex.ru>
2026-05-31 00:10:30 +0300

    empty proj

---
+++ app/app/App.axaml.cs
@@ -0,0 +1,32 @@
+using app.ViewModels;
+using app.Views;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core;
+using Avalonia.Data.Core.Plugins;
+using Avalonia.Markup.Xaml;
+using System.Linq;
+
+namespace app
+{
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ app/app/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace app.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
