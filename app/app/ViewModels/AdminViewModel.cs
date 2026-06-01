4883490825d99fc322a820d4e560aeee8e6d4fe3
dex1fy <idarzamasova@yandex.ru>
2026-06-02 01:08:22 +0300

    add Create and Delete Func

--- app/app/ViewModels/AdminViewModel.cs
+++ app/app/ViewModels/AdminViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using app.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -22,7 +23,7 @@ namespace app.ViewModels
         .Include(p => p.Category)
         .ToList();
 
-        
+
         public List<Product> Products0 => db.Products
         .Include(p => p.ProductType)
         .Include(p => p.Unit)
@@ -44,7 +45,7 @@ namespace app.ViewModels
             switch (sort)
             {
                 case 1:
-                    Products = Products.OrderBy(x=> x.Price).ToList();
+                    Products = Products.OrderBy(x => x.Price).ToList();
                     break;
                 case 2:
                     Products = Products.OrderByDescending(x => x.Price).ToList();
@@ -68,12 +69,12 @@ namespace app.ViewModels
         partial void OnTextFindChanged(string value)
         {
             Products = Products0;
-            Products = Products.Where(x => 
+            Products = Products.Where(x =>
             x.ProductType.Name.Contains(value) ||
             x.Manufacturer.Name.Contains(value) ||
             x.Category.Name.Contains(value) ||
             x.Description.Contains(value)
-            ).ToList(); 
+            ).ToList();
         }
 
         partial void OnSelectedDiscountFilterChanged(int value)
@@ -99,6 +100,31 @@ namespace app.ViewModels
                         .ToList();
                     break;
             }
+        }
+
+        public void CreateProduct()
+        {
+            MainWindowViewModel.Instance.PageSwitcher = new CreateViewModel();
+        }
+
+
+
+        [RelayCommand]
+        public void Delete(Product product)
+        {
+            if (product == null)
+                return;
+
+            db.Products.Remove(product);
+            db.SaveChanges();
+
+            Products = db.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.Unit)
+                .Include(p => p.Supplier)
+                .Include(p => p.Manufacturer)
+                .Include(p => p.Category)
+                .ToList();
         }
     }
 }
--- app/app/ViewModels/CreateViewModel.cs
+++ app/app/ViewModels/CreateViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using app.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace app.ViewModels
 {
-    internal class CreateViewModel : ViewModelBase
+    public partial class CreateViewModel : ViewModelBase
     {
+
+        [ObservableProperty]
+        Product addProduct = new Product()
+        {
+            Price = 0,
+            CurrentDiscount = 0,
+            StockQuantity = 0
+        };
+
+        [ObservableProperty] List<Category> categories = db.Categories.ToList();
+        [ObservableProperty] List<ProductType> type = db.ProductTypes.ToList();
+        [ObservableProperty] List<Unit> units = db.Units.ToList();
+        [ObservableProperty] List<Supplier> suppliers = db.Suppliers.ToList();
+        [ObservableProperty] List<Manufacturer> manufacturers = db.Manufacturers.ToList();
+
+
+        public void Cancel()
+        {
+            MainWindowViewModel.Instance.PageSwitcher = new AdminViewModel();
+        }
+
+        public void Save()
+        {
+            db.Products.Add(AddProduct);
+            db.SaveChanges();
+            MainWindowViewModel.Instance.PageSwitcher = new AdminViewModel();
+        }
+
+
     }
 }
